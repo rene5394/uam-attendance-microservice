4e8b1a1a2f17aba775731a6d2e2096aee1954f1a
reneuam <renet@uassistme.com>
2022-08-23 12:51:04 -0600

    Add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EntryModule } from './api/entry/entry.module';
+import { StatusModule } from './api/status/status.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as any || 'mysql',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT) || 3306,
+      username  : process.env.DATABASE_USERNAME || 'root',
+      password  : process.env.DATABASE_PASSWORD || 'password',
+      database: process.env.DATABASE || 'uamapp',
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: false
+    }),
+    EntryModule,
+    StatusModule],
 })
 export class AppModule {}
