e57008bd5ae74922a868c6fdbbb19987afce79c6
reneuam <renet@uassistme.com>
2022-08-25 09:10:11 -0600

    Change default password for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { StatusModule } from './api/status/status.module';
       host: process.env.DATABASE_HOST || 'localhost',
       port: parseInt(process.env.DATABASE_PORT) || 3306,
       username  : process.env.DATABASE_USERNAME || 'root',
-      password  : process.env.DATABASE_PASSWORD || 'password',
+      password  : process.env.DATABASE_PASSWORD || '',
       database: process.env.DATABASE || 'uamapp',
       entities: [],
       autoLoadEntities: true,
